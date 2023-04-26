b4e48e271dc3e52edc1b32e395107f13ec042c93
Vigneshwaran2605 <91144302+Vigneshwaran2605@users.noreply.github.com>
2023-04-26 16:56:10 +0530

    Update gethash.py

--- certproof-1.0/gethash.py
+++ certproof-1.0/gethash.py
@@ -6,14 +6,14 @@ import cv2
 import pathlib
 from PIL import Image
 client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001/http')
-res = client.add('D:\Blockchain\Mr.Vigneshwaran.N.pdf')
+res = client.add('D:\Blockchain\certificate.pdf')
 hash_value=res['Hash']
 text_file = open("D:\Blockchain\data.txt", "w")
 text_file.write(hash_value)
 text_file.close()
 print(hash_value)
 f = open("data.txt","r")
-im = Image.open(r"D:\Blockchain\certificate.png")
+im = Image.open(r"D:\Blockchain\certiqr.png")
 im.show() 
 qr = qrcode.QRCode(
     version=1,
@@ -30,7 +30,7 @@ img.save("certificate.png")
 
 
 client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001/http')
-res = client.add('D:\Blockchain\Mr.Vigneshwaran.N.pdf')
+res = client.add('D:\Blockchain\certificate.pdf')
 get_hash_value=res['Hash']
 if(get_hash_value==hash_value):
     print("QRcode is generated")
